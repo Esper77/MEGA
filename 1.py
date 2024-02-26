59ff38dbae497c2595d6b27de54a1cb3b692c2d5
Esper77 <yo.moyo12355@gmail.com>
2024-02-26 17:01:48 +0300

    1st task

---
+++ 1.py
@@ -0,0 +1,27 @@
+import csv
+
+
+def calc_total(data):
+    """Расчет общей ценности для каждого привелденного в таблице товара
+
+        Описание переменных:
+        data - входной датасет в формате списка списков
+        new_data - обработанный датасет в виде списка списков (с добавленным total)
+
+    """
+    new_data = [data[0] + ['total']]
+    for category, product, date, price, count in data[1:]:
+        new_data.append([category, product, date, price, count, int(price[:-2]) * int(count[:-2])])
+    return new_data
+
+
+with open('products.csv', encoding='UTF-8') as file:
+    data = [row for row in csv.reader(file, delimiter=";")]
+
+
+with open('products_new.csv', encoding="UTF-8", mode="w") as file:
+    writer = csv.writer(file, delimiter=";")
+    for row in calc_total(data):
+        print(row)
+        writer.writerow(row)
+
