60e547ffeec3299d24a4966a0e148182dd5c5414
Esper77 <yo.moyo12355@gmail.com>
2024-02-26 17:22:05 +0300

    5th task

---
+++ 5.py
@@ -0,0 +1,12 @@
+import csv
+with open('products.csv', encoding='UTF-8') as file:
+    data = [row for row in csv.reader(file, delimiter=";")]
+
+hash_table = {}  # Х
+for row in data[1:]:
+    if row[1] not in hash_table.keys():  # Создание или добавление к уже существующим значения кол-ва товаров
+        hash_table[row[1]] = int(row[4][:-2])
+    else:
+        hash_table[row[1]] += int(row[4][:-2])
+for item in hash_table.items():
+    print(f"{item[0]}, {item[1]}")
