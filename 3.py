c1ab2dc153cf72a69b7d149c1dddb7d2258b3468
Esper77 <yo.moyo12355@gmail.com>
2024-02-26 17:51:24 +0300

    3rd task and some adjustments

---
+++ 3.py
@@ -0,0 +1,26 @@
+import csv
+with open('products.csv', encoding='UTF-8') as file:
+    data = [row for row in csv.reader(file, delimiter=";")]
+
+hash_table = {}
+for row in data[1:]:
+    if row[0] not in hash_table.keys():  # Создание или добавление к уже существующим значения кол-ва товаров
+        hash_table[row[0]] = {row[1]: int(row[4][:-2])}
+    else:
+        if row[1] not in hash_table[row[0]].keys():
+            hash_table[row[0]] = {row[1]: int(row[4][:-2])}
+        else:
+            hash_table[row[0]][row[1]] += int(row[4][:-2])
+
+
+query = ""
+
+while query != "молоко":
+    query = input()
+    if query in hash_table.keys:
+        vals = list(hash_table[query].items())
+        vals.sort(key=lambda a: a[1])
+
+        print(f"В категории: {query} товар: {vals[0][0]} был куплен {vals[0][1]} раз")
+    else:
+        print("Такой категории не существует в нашей БД")
--- 5.py
+++ 5.py
@@ -2,7 +2,7 @@ import csv
 with open('products.csv', encoding='UTF-8') as file:
     data = [row for row in csv.reader(file, delimiter=";")]
 
-hash_table = {}  # Х
+hash_table = {}
 for row in data[1:]:
     if row[1] not in hash_table.keys():  # Создание или добавление к уже существующим значения кол-ва товаров
         hash_table[row[1]] = int(row[4][:-2])
